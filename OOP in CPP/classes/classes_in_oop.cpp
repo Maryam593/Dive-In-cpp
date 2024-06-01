2717b805b3c306bcf263a957658abbaebc69954a
Maryam Saba <maryams91101@gmail.com>
2024-06-01 23:39:57 +0500

    All about classes

--- OOP in CPP/classes/classes_in_oop.cpp
+++ OOP in CPP/classes/classes_in_oop.cpp
@@ -2,14 +2,17 @@
 using namespace std;
 
 class person {
-    public: 
+    //class is a blueprint and object is the instance of the class
+    public:  //access specifier -> three types: public,private, protected
     void display(){
      cout<<"Hello World";
     }
    
 };
 int main(){
+    //obj is object of class
   person obj;
+  //.display is a method
   obj.display();
   return 0;
 }
