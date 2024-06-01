ce870d090904015a16e0bd488082f07cc8d88c75
Maryam Saba <maryams91101@gmail.com>
2024-06-01 16:36:30 +0500

    loops in cpp

---
+++ Intro to cpp/Loops in cpp/loops_in_cpp.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+ int main(){
+    
+    //for loop
+    int b = 0;
+    for(int i = 0; i<=5;i++){
+        cout<<"Hello World"<<endl;
+    }
+    //while loop 
+    int c = 0;
+    while(c<=5){
+        cout<<"hello"<<endl;
+        c++;
+    }
+
+    //do while loop 
+    int d = 0;
+    do{
+        cout<<"hello world how are you :)"<<endl;
+        d++;
+    }
+    while(d<=5);
+    return 0;
+ }
