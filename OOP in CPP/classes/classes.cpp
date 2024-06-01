782785ded79b008becb9f06d160d18de610ca674
Maryam Saba <maryams91101@gmail.com>
2024-06-01 23:36:37 +0500

    Classes in oop

---
+++ OOP in CPP/classes/classes.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+class person {
+    public: 
+    void display(){
+     cout<<"Hello World";
+    }
+   
+};
+int main(){
+  person obj;
+  obj.display();
+  return 0;
+}
