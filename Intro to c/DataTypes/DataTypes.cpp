6bd0937015e7a3361cc1b470dfd3cc05718e8eee
Maryam Saba <maryams91101@gmail.com>
2024-06-01 13:42:46 +0500

    DataTypes

---
+++ Intro to c/DataTypes/DataTypes.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //DataTypes in Cpp
+
+    //******************
+    //Int data type
+    int a = 5;
+   
+    cout<<"the number is = "<<a<<endl;
+    
+
+    //Float Data Type 
+    //area of circle = pi r(Sqr)
+    float pi = 3.14;
+    int r = 2;
+    int multiply = r*r;
+    float result = pi * multiply; 
+    cout<<"the area of circle is " <<result<<endl;
+    
+    //boolean Data type
+    bool c = true;
+    cout<<"the value of c is "<<c<<endl;
+  
+
+//char datatype
+    char b = 'A';
+    cout<<"the value of a is "<<b<<endl;
+    
+
+      return 0;
+}
