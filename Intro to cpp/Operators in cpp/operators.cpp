874a1923ccd318fb3cae2b74afbf898ea2d9e10c
Maryam Saba <maryams91101@gmail.com>
2024-06-01 13:47:56 +0500

    operators in cpp

---
+++ Intro to cpp/Operators in cpp/operators.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+
+//Relational Operators
+int a = 0;
+if(a>18){
+    cout<<"you are welcome to my party"<<endl;
+}
+else{
+    cout<<"you are little kid"<<endl;
+}
+
+
+
+
+    return 0;
+}
